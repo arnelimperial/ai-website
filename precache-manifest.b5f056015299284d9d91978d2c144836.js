7943bbbd2e3021c5002ae2fcd52716285dfefb51
A I <9pagemake7@gmail.com>
2020-10-02 01:44:17 +0300

    Updates

---
+++ precache-manifest.b5f056015299284d9d91978d2c144836.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3763046f28345972054cb45fa6a6490",
+    "url": "/ai-website/index.html"
+  },
+  {
+    "revision": "f0efeff79861ed6bc67d",
+    "url": "/ai-website/static/css/2.68f9a507.chunk.css"
+  },
+  {
+    "revision": "2359317a1a57cc419b36",
+    "url": "/ai-website/static/css/main.9b41df3d.chunk.css"
+  },
+  {
+    "revision": "f0efeff79861ed6bc67d",
+    "url": "/ai-website/static/js/2.d14285d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ai-website/static/js/2.d14285d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2359317a1a57cc419b36",
+    "url": "/ai-website/static/js/main.927d3706.chunk.js"
+  },
+  {
+    "revision": "24549babb7cef267c66b",
+    "url": "/ai-website/static/js/runtime-main.deb96d5f.js"
+  }
+]);
